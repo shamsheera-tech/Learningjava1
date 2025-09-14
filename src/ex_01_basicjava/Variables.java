6b2da9d33c19b19a60797108f36ced6ddcba12a0
shamsheera <s.banu.de@gmail.com>
2025-09-14 13:16:23 +0530

    printf

--- src/ex_01_basicjava/Variables.java
+++ src/ex_01_basicjava/Variables.java
@@ -2,10 +2,6 @@ package ex_01_basicjava;
 
 public class Variables {
     public static void main(String[] args){
-        int a=10;
-        int Enum=12;
-        long l= 5675656L;
-        char c='a';
         int n=9;
    System.out.printf("%d x %d = %d\n",n,1,n*1);
    System.out.printf("%d x %d = %d\n",n,2,n*2);
